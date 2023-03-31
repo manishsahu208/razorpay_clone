c639378d7dc6dd629bdbf52acb189a328c668df3
manish <manishsahu43962@gmail.com>
2023-03-31 09:06:12 +0530

    all files are added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: [ "Mulish" ,"sans-serif" ]
+      },
+      colors:{
+        deepblue:"#02042a",
+        lighblue:"#2b84ea",
+        lightblue300:"#4b94ed",
+        greenlight:"#61cea6",
+        graytext:"#818597",
+        lighgray:"e2e2e2",
+        garyblue:"#344a6c",
+        deepbluehead:"#162f56",
+        gray2:"#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
